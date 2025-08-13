62c87e5f512d13554d1f6204ef9575cb48d99c76
Kamilateixera <KAMILATEIXEIRA98@GMAIL.COM>
2025-08-13 09:50:31 -0300

    Atualiza o codigo para deploy com o reconhecimento de voz no navegador

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from main import traduzir_texto
-import speech_recognition as sr
 
 app = Flask(__name__)
-
-def ouvir_microfone(idioma_origem='pt'):
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("🎤 Diga algo...")
-        try:
-            audio = r.listen(source, timeout=5, phrase_time_limit=10)
-            frase = r.recognize_google(audio, language=idioma_origem)
-            return frase
-        except sr.WaitTimeoutError:
-            return "Erro: Nenhuma fala detectada (tempo limite)"
-        except sr.UnknownValueError:
-            return "Erro: Não foi possível reconhecer a fala"
-        except sr.RequestError as e:
-            return f"Erro ao requisitar o serviço: {e}"
 
 @app.route("/")
 def index():
